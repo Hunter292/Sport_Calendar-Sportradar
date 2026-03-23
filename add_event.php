0dd2ea2009629462dbbc5f0152b5afbb4c90bdf4
Kacper_Cwiek <149629170+Hunter292@users.noreply.github.com>
2026-03-23 21:51:14 +0100

    add single event view

--- add_event.php
+++ add_event.php
@@ -22,6 +22,7 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
 
     if(!isset($_POST["team1"])) $e="<p>Incorrect team selection</p>";
     else{
+        //validate selected teams
         $teams=[];
         $i=1;
         while(isset($_POST["team$i"])){
@@ -34,22 +35,17 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
         $t_results=$query->fetchAll();
         $team_ids=[];
         foreach($t_results as $t_result) array_push($team_ids,$t_result["team_id"]);
-        if(!sizeof($team_ids)==$i-1) $e="<p>Incorrect team selection2</p>";
+        if(!sizeof($team_ids)==$i-1) $e="<p>Incorrect team selection</p>";
     }
     if(!isset($e)){
         //insert event, data validated
         strtotime($_POST["date"])>=time()?$status="scheduled":$status="played";
-        $query=$connection->query("INSERT INTO event values(NULL,{$sport_id["sport_id"]},{$venue_id["venue_id"]},{$competition_id["competition_id"]},NULL,'{$_POST["date"]}','{$_POST["time"]}:00','$status')");
+        $insert="";
+        foreach($team_ids as $team_id) $insert.="$team_id,";
+        //call procedure that inserts into event and teams_playing
+        $query=$connection->prepare("call add_event({$sport_id["sport_id"]},{$venue_id["venue_id"]},{$competition_id["competition_id"]},'{$_POST["date"]}','{$_POST["time"]}:00','$status','$insert',:des)");
+        $query->execute(["des"=>$_POST['description']]);
         if(!$query) $e="<p>Something went wrong</p>";
-        else{
-            $insert="";
-            $query=$connection->query("SELECT event_id FROM event WHERE _venue_id={$venue_id["venue_id"]} and _sport_id={$sport_id["sport_id"]} and date='{$_POST["date"]}' and time='{$_POST["time"]}:00' and _competition_id={$competition_id["competition_id"]}");
-            $id=$query->fetch();
-            $id=$id["event_id"];
-            foreach($team_ids as $team_id) $insert.="(NULL,$id,$team_id,NULL),";
-            $insert[strlen($insert)-1]=';';
-            $query=$connection->query("INSERT INTO teams_playing VALUES $insert");
-        }
     }
 }
 
@@ -62,67 +58,69 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
     <meta name="keywords" content="">
     <meta http-equiv="X-Ua-Compatible" content="IE=edge">
 
-    <link rel="stylesheet" href="main (2).css">
+    <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
 </head>
 
 <body>
-    <div class="container">
-
-        <header>
-            <h1>Sport Calendar</h1>
-            <h3>All sport events in one place</h3>
-        </header>
-        <main>
-        <input type="number" id="num_teams"> <button onclick="generate()">Generate input</button>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="post" >
-            <label>Date</label>
-            <input type="date" name="date"> </br>
-            <label>Time</label>
-            <input type="time" name="time"> </br>
-            <label>Sport</label>
-            <input type="text" name="sport" list="sports"> </br>
-            <label>Venue</label>
-            <input type="text" name="venue" list="venues"> </br>
-            <label>Competition</label>
-            <input type="text" name="competition" list="competitions">
-           
-            <div id="teams_selection"></div>
-            <datalist id="sports">
-                <?php
-                $query=$connection->query("SELECT name from sport");
-                $results=$query->fetchAll();
-                foreach($results as $result) echo "<option value=\"{$result['name']}\">"
-                ?>
-            </datalist> 
-            <datalist id="venues">
-                <?php
-                $query=$connection->query("SELECT name from venue");
-                $results=$query->fetchAll();
-                foreach($results as $result) echo "<option value=\"{$result['name']}\">"
-                ?>
-            </datalist> 
-            <datalist id="teams">
-                <?php
-                $query=$connection->query("SELECT name from team");
-                $results=$query->fetchAll();
-                foreach($results as $result) echo "<option value=\"{$result['name']}\">"
-                ?>
-            </datalist> 
-            <datalist id="competitions">
-                <?php
-                $query=$connection->query("SELECT name from competition");
-                $results=$query->fetchAll();
-                foreach($results as $result) echo "<option value=\"{$result['name']}\">"
-                ?>
-            </datalist> 
+    <div id="wrapper">
+        <?php include("header.html")?>
+        <div class="container">
+            <main>
+            <h2>Add Event</h2>
+            <label>Number of teams/participants</label>
+            <input type="number" id="num_teams"> <button onclick="generate()">Generate input</button>
+            <form action="<?=$_SERVER['PHP_SELF']?>" method="post" >
+                <label>Date</label>
+                <input type="date" name="date"> </br>
+                <label>Time</label>
+                <input type="time" name="time"> </br>
+                <label>Sport</label>
+                <input type="text" name="sport" list="sports"> </br>
+                <label>Venue</label>
+                <input type="text" name="venue" list="venues"> </br>
+                <label>Competition</label>
+                <input type="text" name="competition" list="competitions"> </br>
+                <label>Description</label>
+                <textarea name="description"></textarea>
             
-        </br>
-            <input type="submit" value="Add event">
-            <?=isset($e)?$e:''?>
-        </form>
-        </main>
+                <div id="teams_selection"></div>
+                <datalist id="sports">
+                    <?php
+                    $query=$connection->query("SELECT name from sport");
+                    $results=$query->fetchAll();
+                    foreach($results as $result) echo "<option value=\"{$result['name']}\">"
+                    ?>
+                </datalist> 
+                <datalist id="venues">
+                    <?php
+                    $query=$connection->query("SELECT name from venue");
+                    $results=$query->fetchAll();
+                    foreach($results as $result) echo "<option value=\"{$result['name']}\">"
+                    ?>
+                </datalist> 
+                <datalist id="teams">
+                    <?php
+                    $query=$connection->query("SELECT name from team");
+                    $results=$query->fetchAll();
+                    foreach($results as $result) echo "<option value=\"{$result['name']}\">"
+                    ?>
+                </datalist> 
+                <datalist id="competitions">
+                    <?php
+                    $query=$connection->query("SELECT name from competition");
+                    $results=$query->fetchAll();
+                    foreach($results as $result) echo "<option value=\"{$result['name']}\">"
+                    ?>
+                </datalist> 
+                
+            </br>
+                <input type="submit" value="Add event">
+                <?=isset($e)?$e:''?>
+            </form>
+            </main>
 
+        </div>
     </div>
     <script type="text/javascript" src="javascript.js"></script>
 </body>
---
+++ single_event.php
@@ -0,0 +1,58 @@
+<?php
+if(!$_SERVER["REQUEST_METHOD"]==="GET" || !isset($_GET["e"])){
+    header("Location:index.php");
+    exit();
+}
+require('connect.php');
+$event_id=$_GET["e"];
+$sql='SELECT event_id,date,time,venue.name as v_name,city,country,sport.name as s_name,competition.name as c_name, status,description,capacity
+            FROM event JOIN competition ON competition.competition_id=event._competition_id join sport on sport.sport_id=event._sport_id 
+            join venue ON venue.venue_id=event._venue_id join location on location.location_id=venue._location_id where event_id=:event';
+$query=$connection->prepare($sql);
+$query->execute(["event"=>$event_id]);
+$result=$query->fetch();
+if(!$result){
+    header("Location:index.php");
+    exit();
+}
+$query=$connection->prepare("SELECT name,city,country FROM teams_playing join team on teams_playing._team_id=team.team_id LEFT JOIN location on location.location_id=team._location_id WHERE _event_id=:event");
+$query->execute(["event"=>$event_id]);
+$teams=$query->fetchAll();
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Sport Calendar</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php include("header.html")?>
+        <div class="container">
+            <main>
+                <div>
+                    <div><h3>Details</h3><div><p>Date: <?=$result["date"]?></p><p>Time: <?=substr($result["time"],0,5)?></p><p>Sport: <?=$result["s_name"]?></p><p>Competition: <?=$result["c_name"]?></p></div></div>
+                    <div><h3>Teams/Participants</h3><div>
+                    <?php
+                        foreach($teams as $team){
+                            echo"<p>{$team['name']}</br>{$team['city']} {$team['country']}</p>";
+                        }
+                    ?>
+                    </div></div>
+                    <div><h3>Venue</h3><div><p>City: <?=$result["city"]?></p><p>Country: <?=$result["country"]?></p><p>Capacity: <?=$result["capacity"]?></p></div></div>
+                    <div><h3>Description:</h3><div><p><?=$result["description"]?></p></div></div>
+
+                    
+                </div>
+            </main>
+
+        </div>
+    </div>
+</body>
+</html>
